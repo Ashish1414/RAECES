dc600c6b16e4e163e2e4d09cfdd3e1ce97023870
Ashish1414 <ashubansal.ashishbansal@gmail.com>
2019-10-05 19:15:04 +0530

    navigation drawer added

--- app/src/main/java/com/ngo/raeces/StudentDashboard.java
+++ app/src/main/java/com/ngo/raeces/StudentDashboard.java
@@ -1,22 +1,51 @@
 package com.ngo.raeces;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.location.LocationListener;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class StudentDashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class StudentDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Button btnLogout;
     SharedPreferences sharedPreferences;
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    ActionBarDrawerToggle toggle;
 
+    TextView name;
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
         btnLogout = findViewById(R.id.btnLogout);
+        View ab = navigationView.getHeaderView(0);
+        Menu show = navigationView.getMenu();
+        name = (TextView) ab.findViewById(R.id.name_user);
+        logout = ab.findViewById(R.id.logout);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,5 +60,10 @@ public class StudentDashboard extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
     }
 }
